0939f261888998d74cf574b36ec11008ec59d1a8
wsf <772920866@qq.com>
2014-11-04 00:19:12 +0800

    Signed-off-by: wsf <772920866@qq.com>

--- MAP/数据挖掘/GoogleMap/GMAPTest/Common/PlaceHelper.cs
+++ MAP/数据挖掘/GoogleMap/GMAPTest/Common/PlaceHelper.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GMap.NET;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -16,19 +17,52 @@ namespace GMAPTest.Common
         {
             var list = File.ReadAllLines("D:\\simiao.txt");
             List<string> lines = new List<string>();
-            foreach (var s in list)
+            var lists = GetPoints(list.ToList());
+            foreach (var s in lists)
             {
-                var ps = GoogleHelper.GetPoint(s);
+                lines.Add(s.Geometry.Location.Lat + "," + s.Geometry.Location.Lng);
+            }
+            File.WriteAllLines("D:\\res.txt", lines);
+        }
+        /// <summary>
+        /// 根据地址返回匹配的目标
+        /// </summary>
+        /// <param name="addresses"></param>
+        public static List<GoogleAddress> GetPoints(List<string> addresses)
+        {
+            var list = new List<GoogleAddress>();
+            for (int i = 0; i < addresses.Count; i++)
+            {
+                var ps = GoogleHelper.GetPoint(addresses[i]);
                 if (ps == null || ps.Count <= 0)
                 {
-                    lines.Add("无");
+                    list.Add(new GoogleAddress());
                 }
                 else
                 {
-                    lines.Add(ps.FirstOrDefault().Geometry.Location.Lat + "," + ps.FirstOrDefault().Geometry.Location.Lng);
+                    list.Add(ps.FirstOrDefault());//提取匹配的第一个目标
                 }
             }
-            File.WriteAllLines("D:\\res.txt", lines);
+            return list;
+        }
+
+        /// <summary>
+        /// 根据经纬度列表获取地址列表，取匹配的第一条记录
+        /// </summary>
+        /// <param name="points"></param>
+        /// <returns></returns>
+        public static List<GoogleAddress> GetAddresses(List<PointLatLng> points)
+        {
+            var addresses = new List<GoogleAddress>();
+            foreach (var item in points)
+            {
+                var add = GoogleHelper.GetAddress(item).FirstOrDefault();
+                if (add == null)
+                    addresses.Add(new GoogleAddress());
+                else
+                    addresses.Add(add);
+            }
+            return addresses;
         }
     }
 }
--- MAP/数据挖掘/GoogleMap/GMAPTest/Common/XLSHelper.cs
+++ MAP/数据挖掘/GoogleMap/GMAPTest/Common/XLSHelper.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NPOI.SS.UserModel;
+using GMap.NET;
 
 namespace GMAPTest.Common
 {
@@ -14,16 +15,25 @@ namespace GMAPTest.Common
     /// </summary>
     public class XLSHelper
     {
-        public static void Write2Xls(string path)
+        /// <summary>
+        /// 将结果写到xls中
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="addresses"></param>
+        /// <param name="points"></param>
+        public static void Write2Xls(string path, List<GoogleAddress> addresses, List<PointLatLng> points)
         {
             using (Stream stream = File.OpenWrite(path))
             {
                 HSSFWorkbook book = new HSSFWorkbook();
                 var sheet = book.CreateSheet("Sheet1");
-                SetHeader(sheet, "");
-                var row = sheet.CreateRow(0);
+                var head = new List<string>() { "纬度", "经度", "全称", "简称", "类型","格式化地址" , "区域坐标范围", "坐标类型", "视角范围", "整体类型" };
+                SetHeader(sheet, head.ToArray());
+                foreach (var item in addresses)
+                {
+                    var row = sheet.CreateRow(sheet.LastRowNum);
 
-
+                }
                 book.Write(stream);
             }
             
