e388b0c651343de1da866f26759684658956f357
wsf1990 <772920866@qq.com>
2014-11-03 17:59:18 +0800

    17:59

--- MAP/数据挖掘/GoogleMap/GMAPTest/Common/XLSHelper.cs
+++ MAP/数据挖掘/GoogleMap/GMAPTest/Common/XLSHelper.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NPOI.SS.UserModel;
 
 namespace GMAPTest.Common
 {
@@ -19,12 +20,29 @@ namespace GMAPTest.Common
             {
                 HSSFWorkbook book = new HSSFWorkbook();
                 var sheet = book.CreateSheet("Sheet1");
+                SetHeader(sheet, "");
                 var row = sheet.CreateRow(0);
 
 
                 book.Write(stream);
             }
             
+        }
+        /// <summary>
+        /// 设置XLS头
+        /// </summary>
+        /// <param name="sheet"></param>
+        /// <param name="headers"></param>
+        public static void SetHeader(ISheet sheet, params string[] headers)
+        {
+            var row = sheet.CreateRow(0);
+            int count = headers.Count();
+            for (int i = 0; i < count; i++)
+            {
+                var cell = row.CreateCell(i);
+                cell.SetCellType(CellType.STRING);
+                cell.SetCellValue(headers[i]);
+            }
         }
     }
 }
